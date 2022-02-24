dc46402204e5874648f5c46577cfd452bff4e5c9
suxmxn <soomin1938@daum.net>
2022-02-24 15:50:01 +0900

    스톱워치 화면 만들기

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.androidlab
 
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -51,5 +53,16 @@ class MainActivity : AppCompatActivity() {
         text.setOnClickListener {
             targetView.visibility = View.VISIBLE
         }*/
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                Log.d("kkang", "Touch down event x: ${event.x}, rawX: ${event.rawX}")
+            }
+            MotionEvent.ACTION_UP -> {
+                Log.d("kkang", "Touch up event")
+            }
+        }
+        return super.onTouchEvent(event)
     }
 }
